812aa350763b4bede794bd7eb2695187d0eb8b78
WhiteWhate <lise.bilhaut@gmail.com>
2020-05-30 20:54:54 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/Logo-whiteBG-small.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={logo} alt="logo" />
+        <h1>Connect with your friends & make your life better</h1>
+        <p>~App in development~
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://happychallenge.fun/"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        >Learn more about the Happy Challenge Project!</a>
       </header>
     </div>
   );
